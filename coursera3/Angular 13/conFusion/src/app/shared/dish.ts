6730ff75eafe269cb6a68ddd933902a9ddacf832
nishant9227 <95128550+nishant9227@users.noreply.github.com>
2022-02-17 14:08:30 +0530

    Data Binding

--- coursera3/Angular 13/conFusion/src/app/shared/dish.ts
+++ coursera3/Angular 13/conFusion/src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+ import { Comment } from './comment';
+
  export class Dish {
   id!: string;
   name!: string;
@@ -7,5 +9,9 @@
   label!: string;
   price!: string;
   description!: string;
+
+  comments!:Comment[];
   
  }
+
+ 
